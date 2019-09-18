e04dd4c8b2d6687f2a16b9164ddc5d4ff4a23eb1
Michael Paulukonis <mpauluko@travelers.com>
2019-09-17 20:46:35 -0400

    Playing with the example file As that's the only way I've been able to get this to work. There are a bunch of hard-coded things I don't like. But what I _DO_ like is that it actually works. So.

--- examples/example.js
+++ examples/example.js
@@ -1,5 +1,34 @@
 const { ImageDownloader } = require('../')
+const path = require('path')
+const fs = require('fs')
+const program = require('commander');
+program.version('0.0.1');
 
-const downloader = new ImageDownloader(__dirname)
 
-downloader.downloadImages('big bang theory', 5)
+const core = (terms, limit, target) => {
+    const outpath = path.join(__dirname, target)
+    if (!fs.existsSync(outpath)) {
+        fs.mkdirSync(outpath)
+    }
+
+    const downloader = new ImageDownloader(outpath, true)
+
+    downloader.downloadImages(terms, limit)
+}
+
+function myParseInt(value, dummyPrevious) {
+    // parseInt takes a string and an optional radix
+    return parseInt(value, 10);
+  }
+
+program
+    .option('-d, --debug', 'output extra debugging')
+    .option('-s, --search <string>', 'search and download')
+    .option('-t, --target <string>', 'sub-folder in output to save in', 'sources')
+    .option('-l, --limit <number>', 'number to download', '100', myParseInt)
+
+program.parse(process.argv)
+
+if (program.search) {
+    core(program.search, program.limit, program.target)
+}
